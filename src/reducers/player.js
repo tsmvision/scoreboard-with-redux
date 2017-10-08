ddb14858fd161e64cb75ca7db5fb5583ddfe9d00
luke <lukelee01@gmail.com>
2017-10-08 15:19:51 -0700

    feature: add redux functions.

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,2 +1,17 @@
-import * as PlayerActionTypes from '../action'
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState = [
+    {
+        name: 'Jim Hoskins',
+        score: 31,
+    },
+    {
+        name: 'Andrew Chalkley',
+        score: 20,
+    },
+    {
+        name: 'Alena Holligan',
+        score: 50,
+    },
+];
 
