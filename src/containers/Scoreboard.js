34329073f84f00374c195d4faa4c0347b0769f84
luke <lukelee01@gmail.com>
2017-10-07 17:53:33 -0700

    refactor: clean up Scoreboard codes.

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -4,64 +4,63 @@ import Header from '../components/Header';
 import AddPlayerForm from '../components/AddPlayerForm';
 
 
-const INITIAL_STATE = {
-  players: [
-    {
-      name: 'Jim Hoskins',
-      score: 31,
-    },
-    {
-      name: 'Andrew Chalkley',
-      score: 20,
-    },
-    {
-      name: 'Alena Holligan',
-      score: 50,
-    },
-  ],
-};
+class Scoreboard extends React.Component {
 
-const Scoreboard = React.createClass({
-  getInitialState: function () {
-    return INITIAL_STATE;
-  },
-  onScoreChange: function(index, delta) {
-    this.state.players[index].score += delta;
-    this.setState(this.state);
-  },
+    state = {
+        players: [
+            {
+                name: 'Jim Hoskins',
+                score: 31,
+            },
+            {
+                name: 'Andrew Chalkley',
+                score: 20,
+            },
+            {
+                name: 'Alena Holligan',
+                score: 50,
+            },
+        ],
+    };
 
-  onAddPlayer: function(name) {
-    this.state.players.push({ name: name, score: 0 });
-    this.setState(this.state);
-  },
 
-  onRemovePlayer: function(index) {
-    this.state.players.splice(index, 1);
-    this.setState(this.state);
-  },
+    onScoreChange = (index, delta) => {
+        this.state.players[index].score += delta;
+        this.setState(this.state);
+    };
 
-  render: function() {
-    return (
-      <div className="scoreboard">
-        <Header players={this.state.players} />
-        <div className="players">
-          {this.state.players.map(function(player, index) {
-             return (
-               <Player
-                 name={player.name}
-                 score={player.score}
-                 key={player.name}
-                 onScoreChange={(delta) => this.onScoreChange(index, delta)}
-                 onRemove={() => this.onRemovePlayer(index)}
-               />
-             );
-           }.bind(this))}
-        </div>
-        <AddPlayerForm onAdd={this.onAddPlayer} />
-      </div>
-    );
-  }
-});
+    onAddPlayer = (name) => {
+        this.state.players.push({name: name, score: 0});
+        this.setState(this.state);
+    };
+
+    onRemovePlayer = (index) => {
+        this.state.players.splice(index, 1);
+        this.setState(this.state);
+    };
+
+    render() {
+        return (
+            <div className="scoreboard">
+                <Header players={this.state.players}/>
+                <div className="players">
+                    {this.state.players.map(function (player, index) {
+                        return (
+                            <Player
+                                name={player.name}
+                                score={player.score}
+                                key={player.name}
+                                onScoreChange={(delta) => this.onScoreChange(index, delta)}
+                                onRemove={() => this.onRemovePlayer(index)}
+                            />
+                        );
+                    }.bind(this))}
+                </div>
+                <AddPlayerForm onAdd={this.onAddPlayer}/>
+            </div>
+        );
+    }
+}
 
 export default Scoreboard;
 
