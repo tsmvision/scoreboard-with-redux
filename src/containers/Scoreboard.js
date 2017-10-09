baeacd7d0e70fe21eaaa50618bffc68897aefc0d
luke <lukelee01@gmail.com>
2017-10-08 19:14:01 -0700

    feature: add redux functions.

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { PropTypes } from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as PlayerActionCreators from '../actions/player';
 import Player from '../components/Player';
 import Header from '../components/Header';
 import AddPlayerForm from '../components/AddPlayerForm';
 
 
 class Scoreboard extends React.Component {
+
+    static PropTypes = {
+        player: PropTypes.array.isRequired
+    }
 
     state = {
         players: [
@@ -41,23 +47,29 @@ class Scoreboard extends React.Component {
     };
 
     render() {
+        const { dispatch, players } = this.props;
+        const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
+        const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
+        const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
+
+        const playerComponents = players.map( (player, index) => (
+            <Player
+                index={index}
+                name={player.index}
+                score={player.score}
+                key={player.name}
+                updatePlayerScore={updatePlayerScore}
+                removePlayer={removePlayer}
+            />
+        ) );
+
         return (
             <div className="scoreboard">
-                <Header players={this.state.players}/>
+                <Header players={players}/>
                 <div className="players">
-                    {this.state.players.map(function (player, index) {
-                        return (
-                            <Player
-                                name={player.name}
-                                score={player.score}
-                                key={player.name}
-                                onScoreChange={(delta) => this.onScoreChange(index, delta)}
-                                onRemove={() => this.onRemovePlayer(index)}
-                            />
-                        );
-                    }.bind(this))}
+                    { playerComponents }
                 </div>
-                <AddPlayerForm onAdd={this.onAddPlayer}/>
+                <AddPlayerForm addPlayer={addPlayer}/>
             </div>
         );
     }
